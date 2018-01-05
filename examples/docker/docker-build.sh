20f5aaf999e5ebc04243e45adfbf726aa687289c
Vitor Enes <vitorenesduarte@gmail.com>
2018-01-05 13:08:53 +0000

    examples folder structure

--- examples/docker/docker-build.sh
+++ examples/docker/docker-build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "$0")
+CONTEXT=${DIR}/../
 IMAGE=vitorenesduarte/cal-example
 DOCKERFILE=${DIR}/Dockerfile
 
 # build image
 docker build --no-cache \
   -t ${IMAGE} \
-  -f ${DOCKERFILE} ${DIR}
+  -f ${DOCKERFILE} ${CONTEXT}
 
 # push image
 docker push ${IMAGE}
